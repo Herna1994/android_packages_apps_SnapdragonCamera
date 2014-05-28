69cc229b97fd61a3d8c7216d8528ffab54c3806f
kaiyiz <kaiyiz@codeaurora.org>
2014-05-28 17:01:33 +0800

    Camera2: Make Continuous Shot off when HDR on.
    The HDR function can not existed with the Continuous Shot
    function,so change code to achieve the function like that
    when we open HDR function ,make the Continuous Shot off,
    and we close HDR,make the Continuous Shot on.
    
    CRs-fixed: 667319
    
    Change-Id: I75ab5d268c1feb2d95ff6a6b0f35dcf96d8cc953

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1452,6 +1452,10 @@ public class PhotoModule
                     Integer.toString(mParameters.getSharpness()),
                     colorEffect,
                     sceneMode, redeyeReduction, aeBracketing);
+            if (CameraUtil.SCENE_MODE_HDR.equals(mSceneMode)) {
+                mUI.overrideSettings(CameraSettings.KEY_LONGSHOT,
+                        mActivity.getString(R.string.setting_off_value));
+            }
         } else if (mFocusManager.isZslEnabled()) {
             focusMode = mParameters.getFocusMode();
             overrideCameraSettings(flashMode, null, focusMode,
@@ -1470,6 +1474,9 @@ public class PhotoModule
             String fMode = Parameters.FLASH_MODE_OFF;
             mUI.overrideSettings(CameraSettings.KEY_FLASH_MODE, fMode);
             mParameters.setFlashMode(fMode);
+        }
+        if (Parameters.SCENE_MODE_AUTO.equals(mSceneMode)) {
+            mUI.overrideSettings(CameraSettings.KEY_LONGSHOT, null);
         }
     }
 
