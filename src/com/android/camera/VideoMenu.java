805c4b6df72bf1b5d81102921730e32147aea6ab
Ivan Evlogiev <ivanevlogiev@codeaurora.org>
2014-07-03 14:45:37 +0300

    Camera2: Add See More feature in the menu
    - User can select See More feature in video mode
    
    Change-Id: I13636846cce67caeb6eb0b3e073399fdae2be471

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -82,6 +82,7 @@ public class VideoMenu extends PieController
                 CameraSettings.KEY_POWER_MODE,
                 CameraSettings.KEY_WHITE_BALANCE,
                 CameraSettings.KEY_VIDEO_HIGH_FRAME_RATE,
+                CameraSettings.KEY_SEE_MORE,
                 CameraSettings.KEY_VIDEOCAMERA_FLASH_MODE,
        };
 
