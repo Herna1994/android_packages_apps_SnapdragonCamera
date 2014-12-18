0528bf6738e42d87b88622273d83d989292f4142
Emilian Peev <epeevs@codeaurora.org>
2014-12-18 14:39:54 +0200

    Camera: Avoid 'takePicture()' in preview ready state
     Depending on timing 'takePicture()' could be
     triggered while the preview surface texture is
     still not available. The camera preview in such
     scenario can be started but will not run
     without a valid surface texture. Triggering
     snapshot in this case is not allowed and
     can lead to runtime exception.
    
    CRs-Fixed: 769441
    Change-Id: I6c4f013e9ca8b5903abbfe69394dc0871669d7c2

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2053,7 +2053,8 @@ public class PhotoModule
         if (mPaused || mUI.collapseCameraControls()
                 || (mCameraState == SWITCHING_CAMERA)
                 || (mCameraState == PREVIEW_STOPPED)
-                || (null == mFocusManager)) return;
+                || (null == mFocusManager)
+                || (null == mUI.getSurfaceTexture())) return;
 
         // Do not take the picture if there is not enough storage.
         if (mActivity.getStorageSpaceBytes() <= Storage.LOW_STORAGE_THRESHOLD_BYTES) {
